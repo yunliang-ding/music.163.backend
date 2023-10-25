e89a783deebbcf331bc2425e144ae5db029cd20b
xulei <xulei@lei.com>
2023-10-25 13:22:05 +0800

    fixed

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
   })
 })
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 6900
 const host = process.env.HOST || ''
 
 app.server = app.listen(port, host, () => {
